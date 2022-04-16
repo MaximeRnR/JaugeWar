2718b8d1c42f744930fe4eb3787dd2c058ecbcd5
MaximeRnR <maxime.rnr@gmail.com>
2022-04-16 12:20:56 +0200

    style

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,23 @@ body {
   height: 100vh;
   width: 100vw;
   font-family: 'Roboto', sans-serif;
+
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+}
+
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
 }
 
 #app {
@@ -23,7 +40,7 @@ body {
   justify-content: flex-start;
   align-items: center;
   flex-direction: column;
-  background: whitesmoke;
+  background: transparent;
 }
 
 
--- src/components/JaugeWar.vue
+++ src/components/JaugeWar.vue
@@ -34,7 +34,7 @@ function increaseColor(color: string) {
 </script>
 
 <template>
-  <h1> Jauge War </h1>
+  <h1 class="title"> Jauge War </h1>
   <canvas ref="canvas" width="500" height="500"></canvas>
   <div class="btns-container">
     <button class="increase-btn" style="background: #25A851" @click="increaseColor('top-color')">+1</button>
@@ -44,6 +44,10 @@ function increaseColor(color: string) {
 
 <style scoped>
 
+h1.title {
+  color: white;
+}
+
 .increase-btn {
   width: 50px;
   height: 30px;
@@ -52,7 +56,7 @@ function increaseColor(color: string) {
 
 .btns-container {
   margin-top: 12px;
-  width: 50%;
+  width: 80%;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
@@ -61,15 +65,22 @@ function increaseColor(color: string) {
 
 .btns-container button {
   border-radius: 10px;
+  font-size: 2em;
   font-weight: bold;
   cursor: pointer;
   color: white;
   outline: none;
+  height: 50px;
+  width: 45%;
+  border: 2px solid white;
+}
+
+.btns-container button:active {
   border: none;
 }
 
 canvas {
-  border: 1px solid black;
+  border: 1px solid white;
   border-radius: 50px;
 }
 </style>
